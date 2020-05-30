a931af34304261e66053b720661c7e06b9f17862
Alex <alex90prus@yandex.ru>
2020-05-30 14:41:36 +0300

    history

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
 export default {
-  logout: 'Вы вышли из системы',
+  'logout': 'Вы вышли из системы',
+  'login': 'Для начала войдите в систему',
   'auth/user-not-found': 'Пользователь не найден!',
   'auth/wrong-password': 'Не верный логин или пароль!',
   'auth/email-already-in-use': 'Пользователь с таким Email уже существует!',
