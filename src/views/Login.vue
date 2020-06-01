4dbaa2f69353797549992d9b120c6bbf50348f60
Alex <alex90prus@yandex.ru>
2020-06-01 18:43:09 +0300

    deploy

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -63,6 +63,7 @@
 <script>
 import { email, required, minLength } from 'vuelidate/lib/validators';
 import messages from '../utils/messages';
+import localizeFilter from '../filters/localize.filter';
 
 
 export default {
@@ -84,7 +85,7 @@ export default {
   },
   mounted() {
     if (messages[this.$route.query.message]) {
-      this.$message(messages[this.$route.query.message]);
+      this.$message(localizeFilter(messages[this.$route.query.message]));
     }
   },
   methods: {
